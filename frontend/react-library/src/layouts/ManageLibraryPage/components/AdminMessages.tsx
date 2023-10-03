398ceaf444cdf7d5ef51e350ae6c0c510dc5e951
Michael Aydelotte <123844550+flyfiguy@users.noreply.github.com>
2023-10-03 08:32:18 -0600

    Creation of AdminMessages.tsx file
    Creation of AdminMessages.tsx file
    226, 227

---
+++ frontend/react-library/src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -0,0 +1,69 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from 'react';
+import MessageModel from "../../../models/MessageModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const AdminMessages = () => {
+    //Okta/authentication State
+    const { authState } = useOktaAuth();
+
+    // Normal Loading Pieces
+    const [isLoadingMessages, setIsLoadingMessages] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+    
+    //Messages endpoint state
+    const [messages, setMessages] = useState<MessageModel[]>([]);
+    const [messagesPerPage] = useState(5);
+
+    //Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchUserMessages = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/messages/search/findByClosed/?closed=false&page=${currentPage-1}&size=${messagesPerPage}`;
+                const requestOptions = {
+                    method: 'GET',
+                    hearers: {
+                        Authorization: 'bearer ${authState.accessToken?.accessToken}';
+                        'Content-Type': 'applicaiton/json'
+                    }
+                };
+                const messagesResponse = await fetch(url, requestOptions);
+                if(!messagesResponse.ok){
+                    throw new Error('Something went wrong in AdminMessages.tsx!')
+                }
+                const messagesResponseJson = await messagesResponse.json();
+
+                setMessages(messagesResponseJson._embedded.messages);
+                setTotalPages(messagesResponseJson.page.totalPages);
+            }
+            setIsLoadingMessages(false);
+        }
+        fetchUserMessages().catch((error: any) => {
+            setIsLoadingMessages(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0,0);
+    }, [authState, currentPage]);
+
+    if(isLoadingMessages) {
+        return (
+            <SpinnerLoading/>
+        )
+    }
+
+     if(httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p> 
+            </div>
+        );
+     }
+    
+     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+
+    return ();
+}
