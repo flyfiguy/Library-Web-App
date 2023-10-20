a74bcd1bd9c36c3cb3eba69df9b7625cf7bdabc9
Michael Aydelotte <123844550+flyfiguy@users.noreply.github.com>
2023-10-20 08:26:49 -0600

    Backend controller and service to increase and decrease book quantity
    Backend controller and service to increase and decrease book quantity.
    249 - 252

--- backend/my-spring-boot-library/mySpringBootLibraryApp/src/main/java/com/springbootlibrary/controller/AdminController.java
+++ backend/my-spring-boot-library/mySpringBootLibraryApp/src/main/java/com/springbootlibrary/controller/AdminController.java
@@ -18,6 +18,34 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    /**
+     * <p>Endpoint used to increase a single books quantity<p/>
+     * @param token in the header
+     * @param bookId in the request param
+     * @throws Exception when requester is not an admin based on the token
+     */
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("increaseBookQuantity: Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("decreaseBookQuantity: Administration page only");
+        }
+
+        adminService.decreaseBookQuantity(bookId);
+    }
+
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value="Authorization")String token,
                          @RequestBody AddBookRequest addBookRequest) throws  Exception {
--- backend/my-spring-boot-library/mySpringBootLibraryApp/src/main/java/com/springbootlibrary/service/AdminService.java
+++ backend/my-spring-boot-library/mySpringBootLibraryApp/src/main/java/com/springbootlibrary/service/AdminService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class AdminService {
@@ -15,6 +17,39 @@ public class AdminService {
     @Autowired
     public AdminService (BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public void increaseBookQuantity(Long bookId) throws Exception {
+
+        //Get book from db based on bookId (unique key)
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        //If no book found, throw an exception
+        if(!book.isPresent()) {
+            throw new Exception("increaseBookQuantity: Book not found");
+        }
+
+        //Increase count of books available
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        //Increase overall book count for the book
+        book.get().setCopies(book.get().getCopies()+1);
+
+        //Save book update to repo/database
+        bookRepository.save(book.get());
+
+    }
+
+    public void decreaseBookQuantity(Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(!book.isPresent() || book.get().getCopiesAvailable() <=0 || book.get().getCopies() <= 0) {
+            throw new Exception("decreaseBookQuantity: Either book not found or quantity locked due to either copies available or overall copies being <= 0");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable()-1);
+        book.get().setCopies(book.get().getCopies()-1);
+
+        bookRepository.save(book.get());
     }
 
     public void postBook(AddBookRequest addBookRequest){
