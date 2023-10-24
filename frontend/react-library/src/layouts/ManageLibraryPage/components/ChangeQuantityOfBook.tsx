9c4ddc6f162e13727ab714bd4a4ed596d1e7d430
Michael Aydelotte <126712416+aydelotte-lds@users.noreply.github.com>
2023-10-24 08:29:21 -0600

    Frontend - Added functionality to increase and decrease quantity of books
    Frontend - Added functionality to increase and decrease quantity of books
    256, 257

--- frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
+import { useOktaAuth } from "@okta/okta-react";
 
 export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) => {
     
+    //State
+    const { authState } = useOktaAuth();
     const [quantity, setQuantity] = useState<number>(0);
     const [remaining, setRemaining] = useState<number>(0);
 
@@ -13,6 +16,49 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
         };
         fetchBookInState();
     } ,[])
+
+    async function increaseQuantity() {
+        const url = `http://localhost:8080/api/admin/secure/increase/book/quantity?bookId=${props.book.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type' : 'application/json'
+            }
+        };
+
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if(!quantityUpdateResponse.ok) {
+            throw new Error('ChangeQuantityOfBook.increaseQuantity: Something went wrong!')
+        }
+
+        //Successfully called api so we update. 
+        //No error means the call to the backend worked. No need to call useEffect for this.
+        setQuantity(quantity + 1);
+        setRemaining(remaining + 1)
+    }
+
+    async function decreaseQuanity() {
+        const url = `http://localhost:8080/api/admin/secure/decrease/book/quantity?bookId=${props.book.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type' : 'application/json'
+            }
+        };
+
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if(!quantityUpdateResponse.ok) {
+            throw new Error('ChangeQuantityOfBook.decreaseQuanity: Something went wrong!')
+        }
+
+        //Successfully called api so we update. 
+        //No error means the call to the backend worked. No need to call useEffect for this.
+        setQuantity(quantity - 1);
+        setRemaining(remaining - 1)
+       
+    }
 
     return (
         <div className="card mt3-3 shadow p-3 mb-3 bg-body rounded">
@@ -55,8 +101,8 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
                         <button className="m-1 btn btn-md btn-danger">Delete</button>
                     </div>
                 </div> 
-                <button className="m1 btn btn-md main-color text-white">Add Quantity</button>   
-                <button className="m1 btn btn-md btn-warning">Descrease Quantity</button>
+                <button className="m1 btn btn-md main-color text-white" onClick={increaseQuantity}>Add Quantity</button>   
+                <button className="m1 btn btn-md btn-warning" onClick={decreaseQuanity}>Descrease Quantity</button>
             </div>
         </div>
     );
