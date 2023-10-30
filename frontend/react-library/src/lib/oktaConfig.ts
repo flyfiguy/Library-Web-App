69f6000ac4bc221fccb06fa1f75fcc8f0795738b
Michael Aydelotte <123844550+flyfiguy@users.noreply.github.com>
2023-10-30 08:46:46 -0600

    Change redirectUri (back to app) in OktaConfig to https
    Change redirectUri (back to app) in OktaConfig to https. Made further changes to my Okta account to make	Applications > Applications > log in and log out redirects https as well as Security > API > Trusted Origins URL to https.
    263 - 265

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -3,7 +3,7 @@
 export const oktaConfig = {
     clientId: '0oa8cijs16D8MP3SF5d7',
     issuer: 'https://dev-42693214.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid','profile','email'],
     pkce: true,
     disableHttpsCheck: true
